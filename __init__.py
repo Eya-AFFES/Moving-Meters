728c8e88f2416b43b3ed3a81c21d8fd39a8fad94
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-13 17:37:11 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,20 @@ ser00 = serial.Serial ("/dev/ttyS0", 9600) #Open port with baud rate
 LOGGER = getLogger(__name__)
 
 class SpeakmeterSkill(MycroftSkill):
-    @intent_handler(IntentBuilder("").require("Speak").require("Words"))
+  
+    def __init__(self):
+        #This method is the constructor, and the key function it has is to define the name of the Skill.
+        super(SpeakmeterSkill, self).__init__(name="SpeakmeterSkill")
+        
+    def initialize(self):
+        MV_F_intent = IntentBuilder("MVFIntent").require("MVFKeyword").optionally("words").build()
+        self.register_intent(MV_F_intent ,self.handle_MV_F_intent)
+    
+    def handle_MV_F_intent(self, message):
+        self.speak_dialog("MV.F")
+        msg="MVF"
+        ser00.write(bytes(msg, 'utf-8'))    
+    
     def speak_back(self, message):
         """
             Repeat the utterance back to the user.
