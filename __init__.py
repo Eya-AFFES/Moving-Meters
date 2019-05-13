29253872d1e1d66279d82344eb3bd9155b04e5ad
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-13 16:36:10 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,34 +1,23 @@
 # Copyright 2016 Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import re
 from os.path import dirname, join
 
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
-
+from mycroft.util.log import getLogger
+import serial
+ser00 = serial.Serial ("/dev/ttyS0", 9600) #Open port with baud rate
 
 # TODO - Localization
+LOGGER = getLogger(__name__)
 
-class SpeakSkill(MycroftSkill):
+class SpeakmeterSkill(MycroftSkill):
     @intent_handler(IntentBuilder("").require("Speak").require("Words"))
     def speak_back(self, message):
         """
             Repeat the utterance back to the user.
 
-            TODO: The method is very english centric and will need
-                  localization.
         """
         # Remove everything up to the speak keyword and repeat that
         utterance = message.data.get('utterance')
@@ -40,4 +29,4 @@ class SpeakSkill(MycroftSkill):
 
 
 def create_skill():
-    return SpeakSkill()
+    return SpeakmeterSkill()
