ed7444013be9ee2e9030ae5b3f87945ff23b3876
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-14 14:49:15 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -51,6 +51,7 @@ class SpeakmeterSkill(MycroftSkill):
         ser00.write(bytes(msg, 'utf-8'))  
  """
     def handle_MV_F_intent(self, message):
+        eya="required meters"
         dist=str(message.data.get("Mydistance"))
         T = ("1","one","2","two","3","three","4","four","5","five","6","six","7","seven","8","eight","9","nine")
         if (T[0] in dist) or (T[1] in dist):
