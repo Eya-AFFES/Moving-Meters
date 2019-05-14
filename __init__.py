6f8a8671526759817ff7bac575126d98e0430352
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-14 14:07:11 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,8 +23,8 @@ class SpeakmeterSkill(MycroftSkill):
         self.register_intent(MV_F_intent ,self.handle_MV_F_intent)
     
     def handle_MV_F_intent(self, message):
-        self.speak_dialog("MV.F")
-        self.speak(message.data.get("Mydistance"))
+        self.speak_dialog("MV.F", message.data.get("Mydistance"))
+        #self.speak(message.data.get("Mydistance"))
         dist=str(message.data.get("Mydistance"))
         msg="MF"
         T = ("1","one","2","two","3","three","4","four","5","five","6","six","7","seven","8","eight","9","nine")
