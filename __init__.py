872e65775f580aba4fb39053ec0b29456c0a6390
Eya-AFFES <37873048+Eya-AFFES@users.noreply.github.com>
2019-05-15 06:28:05 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,32 +22,36 @@ class SpeakmeterSkill(MycroftSkill):
         self.register_intent(MV_F_intent ,self.handle_MV_F_intent)
     
     def handle_MV_F_intent(self, message):
-        meters="meters"
-        dist=str(message.data.get("Mydistance"))
-        T = ("1","one","2","two","3","three","4","four","5","five","6","six","7","seven","8","eight","9","nine")
-        if (T[0] in dist) or (T[1] in dist):
-          msg="1"
-          meters="meter"
-        elif (T[2] in dist) or (T[3] in dist):
-          msg="2"
-        elif (T[4] in dist) or (T[5] in dist):
-          msg="3"
-        elif (T[6] in dist) or (T[7] in dist):
-          msg="4"  
-        elif (T[8] in dist) or (T[9] in dist):
-          msg="5"
-        elif (T[10] in dist) or (T[11] in dist):
-          msg="6"  
-        elif (T[12] in dist) or (T[13] in dist):
-          msg="7" 
-        elif (T[14] in dist) or (T[15] in dist):
-          msg="8" 
-        elif (T[16] in dist) or (T[17] in dist):
-          msg="9" 
-        else: 
-          msg="0"  
-        self.speak_dialog("MV.F", data={'nbr':msg, 'meters':meters})
-        ser00.write(bytes("MF"+msg+"\n", 'utf-8'))
+            try:
+                meters="meters"
+                dist=str(message.data.get("Mydistance"))
+                T = ("1","one","2","two","3","three","4","four","5","five","6","six","7","seven","8","eight","9","nine")
+                if (T[0] in dist) or (T[1] in dist):
+                    msg="1"
+                    meters="meter"
+                elif (T[2] in dist) or (T[3] in dist):
+                    msg="2"
+                elif (T[4] in dist) or (T[5] in dist):
+                    msg="3"
+                elif (T[6] in dist) or (T[7] in dist):
+                    msg="4"  
+                elif (T[8] in dist) or (T[9] in dist):
+                    msg="5"
+                elif (T[10] in dist) or (T[11] in dist):
+                    msg="6"  
+                elif (T[12] in dist) or (T[13] in dist):
+                    msg="7" 
+                elif (T[14] in dist) or (T[15] in dist):
+                    msg="8" 
+                elif (T[16] in dist) or (T[17] in dist):
+                    msg="9" 
+                else: 
+                    msg="0"  
+                self.speak_dialog("MV.F", data={'nbr':msg, 'meters':meters})
+                ser00.write(bytes("MF"+msg+"\n", 'utf-8'))
+                pass
+            except:
+                self.speak_dialog("error")
 
     def stop(self):
         pass
